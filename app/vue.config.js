b719f63b7cfff7efd5893cbc2445a538093d8890
Enzo Costa <enzoengsoftware@gmail.com>
2025-04-21 02:10:23 -0300

    Configs

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,4 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/pokeAPI-Vue/' // O nome do seu repositório GitHub
+    : '/'
+}
+
